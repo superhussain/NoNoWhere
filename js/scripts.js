d3377725bb3480089c34a87b7e210330f4660d83
Hussain Abbas <hussain@hussainabbas.com>
2016-01-16 23:25:39 -0500

    js remove random script

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,8 @@
 (function ($) {
   jQuery(document).ready(function ($) {
-    nav();
     scroll();
   });
 })(jQuery);
-
-nav = function() {
-  $(window).on('scroll', function() {
-    $('.navbar').removeClass('no-stick').addClass('sticky-nav').addClass('stuck');
-  });
-
-  var $logo = $('.logo');
-  $logo.click(function(e) {
-    $('body,html').animate({
-      scrollTop: "0"
-    }, 500);
-    e.preventDefault();
-  });
-}
 
 scroll = function () {
   $(".scroll").click(function (event) { // When a link with the .scroll class is clicked
