76b1eeb449ed5704ebe290cde78970456db4d1bb
Hussain Abbas <hussain@hussainabbas.com>
2016-01-16 23:20:42 -0500

    js scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 (function ($) {
   jQuery(document).ready(function ($) {
-    //scripts
+    nav();
+    scroll();
   });
 })(jQuery);
 
+nav = function() {
+  $(window).on('scroll', function() {
+    $('.navbar').removeClass('no-stick').addClass('sticky-nav').addClass('stuck');
+  });
+
+  var $logo = $('.logo');
+  $logo.click(function(e) {
+    $('body,html').animate({
+      scrollTop: "0"
+    }, 500);
+    e.preventDefault();
+  });
+}
+
+scroll = function () {
+  $(".scroll").click(function (event) { // When a link with the .scroll class is clicked
+    event.preventDefault(); // Prevent the default action from occurring
+    $('html,body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 500); // Animate the scroll to this link's href value
+  });
+}
